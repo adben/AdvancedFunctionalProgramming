7c6cdf8831c23155b18a6c812cf5c4211e7c639b
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2019-08-27 17:18:20 +0200

    AFP: workaround for first round

--- lambda.hs
+++ lambda.hs
@@ -1,9 +1,8 @@
 -- lambda evaluation using De Bruijn index
-type Index = (String, Int)
-
+-- see https://en.wikipedia.org/wiki/De_Bruijn_index
 data Term  = App Term Term
-  | Var String
   | Lam String Term
+  | Var String
   | Num Int
   deriving (Eq, Show)
 
@@ -16,11 +15,12 @@ pprint (App e e') = maybeEnclose e ++ " " ++ maybeEnclose e'
 
 maybeEnclose :: Term -> String
 maybeEnclose (Var n) = n
+maybeEnclose (Num n) = show n
 maybeEnclose x       = "(" ++ pprint x ++ ")"
 
 pprint' x = putStrLn $ pprint x
 
-findBy :: [Index] -> String -> Int
+findBy :: [(String, Int)] -> String -> Int
 findBy ((x, y) : xs) x' = if x' == x then y else findBy xs x'
 findBy _ _              = error " not found "
 
@@ -28,11 +28,16 @@ bruijn :: Term -> Term
 bruijn e = go [] e where
     go m (Lam x e)  = Lam "" $ let m' = map (\(x, y) -> (x, y + 1)) m in go ((x, 0) : m') e
     go m (App e e') = App (go m e) (go m e')
-    go m (Num x)    = Num $ show $ findBy m x
+    go m (Num x)    = Var $ show $ findBy m (show x) --FIXME 
     go m (Var n)    = Var $ show $ findBy m n
 
---TODO finish me
+-- let expr = (Lam "s" (Lam "a" (Var "a"))) in pprint' expr >> pprint' (bruijn expr)
+-- λs.λa.a
+-- λ.λ.0
 
+-- let expr = (Lam "s" (Lam "z" (Num 4))) in pprint' expr >> pprint' (bruijn expr)
+-- λs.λz.4
 
--- (App (Lamb "x" (Var "x")) (Num 3))
--- let exp = (App "x" (lamb "x")(Var 3)) in pprint' exp >> pprint' (bruijn exp)
+-- let expr = (Lam "m" (Lam "n" (Lam "s" (Lam "z" (App (App (Var "m") (Var "s")) (App (App (Var "n") (Var "z")) (Var "s"))))))) in pprint' expr >> pprint' (bruijn expr)
+-- λm.λn.λs.λz.(m s) ((n z) s)
+-- λ.λ.λ.λ.(3 1) ((2 0) 1)
