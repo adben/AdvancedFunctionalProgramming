b79bb2ab8cdc940bff6993f5aaf5f44aee3fc80e
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2019-08-28 12:33:37 +0200

    AFP: contracts, using dante

--- GeneralizedAlgebraicDataType.hs
+++ GeneralizedAlgebraicDataType.hs
@@ -1,5 +1,9 @@
 {- see https://en.wikipedia.org/wiki/Generalized_algebraic_data_type -}
-{-# LANGUAGE DataKinds, KindSignatures, GADTs, StandaloneDeriving, TypeFamilies #-}
+{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE GADTs              #-}
+{-# LANGUAGE KindSignatures     #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeFamilies       #-}
 data Lam :: * -> * where
   Lift :: a                     -> Lam a        -- ^ lifted value
   Pair :: Lam a -> Lam b        -> Lam (a, b)   -- ^ product
@@ -14,5 +18,32 @@ eval (Lam f)    = \x -> eval (f (Lift x))
 eval (App f x)  = (eval f) (eval x)
 eval (Fix f)    = (eval f) (eval (Fix f))
 
+-- >>>  eval(fact)(10)
+-- 3628800
 fact = Fix (Lam (\f -> Lam (\y -> Lift (if eval y == 0 then 1 else eval y * (eval f) (eval y - 1)))))
---use eval(fact)(10)
+
+
+-- Contracts
+data Contract :: * -> * where
+  Pred :: (a -> Bool) -> Contract a
+  Fun  :: Contract a -> Contract b -> Contract (a -> b)
+  DFun :: Contract a -> (a -> Contract b) -> Contract (a -> b)
+
+assert :: Contract a -> a -> a
+assert (Pred p)       x = if p x then x else error "contract violation"
+assert (Fun pre post) f = assert post . f . assert pre
+assert (DFun pre post) f = assert post .
+
+pos :: (Num a, Ord a) => Contract a
+pos = Pred (> 0)
+
+-- >>> assert pos 2 == 2
+-- True
+
+-- >>> assert pos 0 == 1
+-- *** Exception: contract violation
+
+true ::(Num  a, Ord a) => Contract a
+true  = undefined
+
+-- >>> assert true x == x
