f9741be5ca6f7ed31aa6953014ab14debba7a441
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2019-08-29 09:49:23 +0200

    AFP: fixed factorial tests

--- GeneralizedAlgebraicDataType.hs
+++ GeneralizedAlgebraicDataType.hs
@@ -4,12 +4,15 @@
 {-# LANGUAGE KindSignatures     #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeFamilies       #-}
+
 data Lam :: * -> * where
-  Lift :: a                     -> Lam a        -- ^ lifted value
-  Pair :: Lam a -> Lam b        -> Lam (a, b)   -- ^ product
-  Lam  :: (Lam a -> Lam b)      -> Lam (a -> b) -- ^ lambda abstraction
-  App  :: Lam (a -> b) -> Lam a -> Lam b        -- ^ function application
-  Fix  :: Lam (a -> a)          -> Lam a        -- ^ fixed point
+  Lift :: a -> Lam a -- ^ lifted value
+  Pair :: Lam a -> Lam b -> Lam (a, b) -- ^ product
+  Lam :: (Lam a -> Lam b) -> Lam (a -> b) -- ^ lambda abstraction
+  App :: Lam (a -> b) -> Lam a -> Lam b -- ^ function application
+  Fix :: Lam (a -> a) -> Lam a -- ^ fixed point
+  LitI :: Int -> Lam Int
+ --  IsZero ::(a -> Int) -> Lam a
 
 eval :: Lam t -> t
 eval (Lift v)   = v
@@ -17,23 +20,40 @@ eval (Pair l r) = (eval l, eval r)
 eval (Lam f)    = \x -> eval (f (Lift x))
 eval (App f x)  = (eval f) (eval x)
 eval (Fix f)    = (eval f) (eval (Fix f))
+eval (LitI n)   = n
+-- eval (IsZero n) = (eval n
 
--- >>>  eval(fact)(10)
+-- >>> eval(fact)(0)
+-- 1
+
+-- >>> eval(fact)(10)
 -- 3628800
-fact = Fix (Lam (\f -> Lam (\y -> Lift (if eval y == 0 then 1 else eval y * (eval f) (eval y - 1)))))
-
+fact =
+  Fix
+    (Lam
+       (\f ->
+          Lam
+            (\y ->
+               Lift
+                 (if eval y == 0
+                    then 1
+                    else eval y * (eval f) (eval y - 1)))))
 
 -- Contracts
 data Contract :: * -> * where
   Pred :: (a -> Bool) -> Contract a
-  Fun  :: Contract a -> Contract b -> Contract (a -> b)
+  Fun :: Contract a -> Contract b -> Contract (a -> b)
   DFun :: Contract a -> (a -> Contract b) -> Contract (a -> b)
 
+--(==>) :: Contract a -> Contract b -> Contract (a -> b)
 assert :: Contract a -> a -> a
-assert (Pred p)       x = if p x then x else error "contract violation"
+assert (Pred p) x =
+  if p x
+    then x
+    else error "contract violation"
 assert (Fun pre post) f = assert post . f . assert pre
-assert (DFun pre post) f = assert post .
 
+--assert (DFun pre post) f = assert post . assert pre . f
 pos :: (Num a, Ord a) => Contract a
 pos = Pred (> 0)
 
@@ -42,8 +62,10 @@ pos = Pred (> 0)
 
 -- >>> assert pos 0 == 1
 -- *** Exception: contract violation
+-- CallStack (from HasCallStack):
+--   error, called at /var/folders/qj/l2fx79l139724lqywzmrjr540000gn/T/danterMGFu7.hs:50:10 in main:Main
 
-true ::(Num  a, Ord a) => Contract a
-true  = undefined
 
+true :: (Ord a) =>  Contract a
+true = undefined
 -- >>> assert true x == x
